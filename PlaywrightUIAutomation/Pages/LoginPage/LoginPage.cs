1e9b6b0016d40f1d9ea9517c17ef40745ed21367
anuja-p-lohkare <anuja.p.lohkare@gmail.com>
2025-01-02 19:37:37 +0530

    refactor: add alternate way for locators

--- PlaywrightUIAutomation/Pages/LoginPage/LoginPage.cs
+++ PlaywrightUIAutomation/Pages/LoginPage/LoginPage.cs
@@ -1,20 +1,20 @@
 ﻿using Microsoft.Playwright;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PlaywrightUIAutomation.Pages.LoginPage
 {
     public class LoginPage
     {
         private IPage _page;
-        private  ILocator _lnkLogin => _page.Locator("text=Login");
-        private ILocator _txtUserName => _page.Locator("#UserName");
-        private ILocator _txtPassword => _page.Locator("#Password");
-        private ILocator _loginBtn => _page.Locator("text=Log in");
-        private ILocator _employeeDetailsLink => _page.Locator("text='Employee Details'");
+        //private  ILocator _lnkLogin => _page.Locator("text=Login");
+        private  ILocator _lnkLogin => _page.GetByRole(AriaRole.Link, new PageGetByRoleOptions() { Name = "Login"});
+        //private ILocator _txtUserName => _page.Locator("#UserName");
+        private ILocator _txtUserName => _page.GetByLabel("UserName");
+        //private ILocator _txtPassword => _page.Locator("#Password");
+        private ILocator _txtPassword => _page.GetByLabel("Password");
+        //private ILocator _loginBtn => _page.Locator("text=Log in");
+        private ILocator _loginBtn => _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions() { Name = "Log in"});
+        //private ILocator _employeeDetailsLink => _page.Locator("text='Employee Details'");
+        private ILocator _employeeDetailsLink => _page.GetByRole(AriaRole.Link, new PageGetByRoleOptions() { Name = "Employee Details" });
         public LoginPage(IPage page) => _page = page;
 
         public async Task ClickOnLoginOption() => await _lnkLogin.ClickAsync();
